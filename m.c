3e533f3e520440fcc6cc519ee5acae0e2cc50c8d
Danielecarn <dannygoncalves41@gmail.com>
2021-10-27 17:07:27 -0300

    Leitura e escrita do vetor de numeros

---
+++ m.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+void swap(int *a, int *b){ 
+    int temp = *a; 
+    *a = *b; 
+    *b = temp; 
+} 
+void bubbleSort(int *v, int n){ 
+    if (n < 1)return; 
+ 
+    for (int i=0; i<n; i++) 
+        if (v[i] > v[i+1]) 
+            swap(&v[i], &v[i+1]);  
+    bubbleSort(v, n-1); 
+} 
+
+int main(){
+    int tam,i,*v;
+    scanf("%d",&tam);
+    v=(int*)malloc(tam*sizeof(int));
+    for(i=0;i<tam;i++)scanf("%d",&v[i]);
+    bubbleSort(v,tam-1);
+    for(i=0;i<tam;i++)printf("%d ",v[i]);
+    return 0;
+}
+/*
+    char caractere[2];
+    int k = 0;
+    while(fgets(caractere, 2, lerN) != NULL){
+        letras[k] = caractere[k];
+        k++;
+    }
+    printf("Letras:\n");
+    for(i=0;i<qtd_linhas;i++)printf("%c",letras[i]);
+
+    
+    for(int b=0; b<=qtd_linhas; b++){
+        fscanf(lerL, "%c", &l);
+        letras[b] = l;
+    }
+    printf("Letras:\n");
+    for(i=0;i<qtd_linhas;i++)printf("%c",letras[i]);
--- main_ordenacao.c
+++ main_ordenacao.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+//#include "bubble_sort.h"
 int main(){
     FILE *file;
     file = fopen("teste.txt","r");
 
     int i;
     char c;
+    int qtd_linhas=0;
     FILE *arq1,*arq2;
     
     while(!feof(file)){
@@ -16,10 +18,52 @@ int main(){
 
         arq2 = fopen("numeros.txt","a");
         fprintf(arq2,"%i\n", i);
+        
+        qtd_linhas++;
     } 
+    printf("Linhas = %d\n", qtd_linhas);
     
+    int *numeros;
+    int j;
+    numeros=(int*)malloc(qtd_linhas*sizeof(int));
+    FILE *ler = fopen("numeros.txt","r");
+
+    for(int a=0; a<=qtd_linhas; a++){
+        fscanf(ler, "%i", &j);
+        numeros[a] = j;
+    }
+
+    printf("%d ",numeros[i]);
+    for(i=0;i<qtd_linhas;i++)printf("%d ",numeros[i]);
+
+
+    /*
+    int letras[qtd_linhas];
+    int numeros[qtd_linhas];
+    
+    for(int i=0; i<=qtd_linhas; i++){
+        letras[i] = *arq1;
+        numeros[i] = *arq2;
+    }
+    
+    for(int j=0; j<= qtd_linhas; j++){
+        printf("%d", numeros[j]);
+        printf("\n");
+    }
+    for(int j=0; j<= qtd_linhas; j++){
+        printf("%c", letras[j]);
+        printf("\n");
+    }
+        int vetor[12]={2,5,35,6,3,2,1,36,6,0,4,7};
+    int *p = vetor;
+    int tamanho=11;
+    */
+
+    
+
     fclose(file);
     fclose(arq1);
     fclose(arq2);
+    fclose(ler);
     return 0;
 }

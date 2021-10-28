5a719b29c25a2cb2ba0ada6e2c30734594aca00f
Danielecarn <dannygoncalves41@gmail.com>
2021-10-28 16:08:13 -0300

    Desafio finalizado

--- main_ordenacao.c
+++ main_ordenacao.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void troca(int *v, int *prox){ 
     int aux = *v; 
@@ -40,6 +41,8 @@ int main(){
     printf("Linhas = %d\n", qtd_linhas);
     fclose(arq1);
     fclose(arq2);
+    fclose(file);
+
     //Preenchendo vetor de números
     int numeros[qtd_linhas]; 
     int j;
@@ -49,24 +52,51 @@ int main(){
         fscanf(ler, "%i", &j);
         numeros[a] = j;
     }
-
+    /*
     printf("Numeros Embaralhados:\n");
     for(i=0;i<qtd_linhas;i++)printf("%d ",numeros[i]); 
-   
-    copiar = fopen("ordemcrescente.txt", "w+");
+    */
+    copiar = fopen("numeros.txt", "w+");
     bubble_sort(numeros, qtd_linhas);
+    /*
     printf("\nNumeros na ordem crescente:\n");
     for(int i=0;i<qtd_linhas;i++)printf("%d ",numeros[i]);
+    */
     for(int i=0;i<qtd_linhas;i++){
         fprintf(copiar,"%d\n",numeros[i]);
     }
-
-    //Preenchendo vetor de letras
-
-    fclose(file);
-
     fclose(ler); 
     fclose(copiar);
+
+    //Preenchendo vetor de letras
+    FILE *l = fopen("letras.txt","r");
+    char caractere[qtd_linhas];
+    caractere[qtd_linhas-1] ='\0';
+    int letras[qtd_linhas];
+    
+    for(int a=0; a<qtd_linhas; a++){
+        fgets(caractere,qtd_linhas,l);
+        printf("letras: %s",caractere);
+        
+        letras[a]= (int)caractere[0];
+        printf("ascii: %d\n\n",letras[a]);
+    }
+
+    
+    printf("Letras:\n");
+    for(i=0;i<qtd_linhas;i++)printf("%i ",letras[i]); 
+    FILE *copiaL = fopen("letras.txt", "w+");
+    bubble_sort(letras, qtd_linhas);
+    printf("Letras na ordem crescente:\n");
+    for(int i=0;i<qtd_linhas;i++)printf("%c ",letras[i]);
+    for(int i=0;i<qtd_linhas;i++){
+        //char l[qtd_linhas] = letrasN[i]+'0';
+        fprintf(copiaL,"%c\n",letras[i]);
+    }
+
+    fclose(copiaL);
+
+    fclose(l);
     
     return 0;
 }

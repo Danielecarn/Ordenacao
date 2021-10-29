0bf16dd3e63de853686ac406c53b9a9d1f156ec8
Daniele Carnaúba <47640172+Danielecarn@users.noreply.github.com>
2021-10-29 14:52:00 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,86 +1,85 @@
 #include <stdio.h>
-#include "bubbleSort.h"
+#include "bubble_sort.h"
 
 int main(){ 
     
-    int qtd_linhas=0; char auxCaractere; int auxNumero;    
-    int opcaoUsuario = 1;
+    int qtd_linhas=0; char aux_caractere; int aux_numero;    
+    int opcao_usuario = 1;
 
-    while( opcaoUsuario != 0){
+    while( opcao_usuario != 0){
         printf("============== Ordena Tudo ==============\n\n");
         printf("Digite o número da opção desejada: \n\n1 - Separar letras e números.\n2 - Ordenar arquivo.\n0 - Exit.\n\n");
-        scanf("%i", &opcaoUsuario);
+        scanf("%i", &opcao_usuario);
 
-        if(opcaoUsuario < 0 || opcaoUsuario > 2 ){
+        if(opcao_usuario < 0 || opcao_usuario > 2 ){
             printf("Opção inexistente! Por favor escolha outra opção \n\n");
         }
-        if(opcaoUsuario == 1){
-            FILE *arquivoGeral, *arquivoLetras, *arquivoNumeros;
+        if(opcao_usuario == 1){
+            FILE *arquivo_geral, *arq_letras, *arq_numeros;
 
-            arquivoGeral = fopen("teste.txt","r");
-            arquivoLetras = fopen("letras.txt", "w+");
-            arquivoNumeros = fopen("numeros.txt","w+");
+            arquivo_geral = fopen("teste.txt","r");
+            arq_letras = fopen("letras.txt", "w+");
+            arq_numeros = fopen("numeros.txt","w+");
                     
             //Preenchendo arquivos de letras e números
-            while(!feof(arquivoGeral)){
-                fscanf(arquivoGeral,"%c\t%i\n", &auxCaractere,&auxNumero);
+            while(!feof(arquivo_geral)){
+                fscanf(arquivo_geral,"%c\t%i\n", &aux_caractere,&aux_numero);
             
-                fprintf(arquivoLetras,"%c\n", auxCaractere);
-                fprintf(arquivoNumeros,"%i\n", auxNumero);
+                fprintf(arq_letras,"%c\n", aux_caractere);
+                fprintf(arq_numeros,"%i\n", aux_numero);
             
                 qtd_linhas++;
             }
             printf("\nLetras e Números separados!\n\n");   
 
-            fclose(arquivoLetras);
-            fclose(arquivoNumeros);
-            fclose(arquivoGeral);
+            fclose(arq_letras);
+            fclose(arq_numeros);
+            fclose(arquivo_geral);
         }
-        if(opcaoUsuario == 2){  
-            int vetorNumeros[qtd_linhas]; 
+        if(opcao_usuario == 2){  
+            int vetor_numeros[qtd_linhas]; 
             char caractere[qtd_linhas];
             caractere[qtd_linhas-1] ='\0';
 
             //Preenchendo vetor de números 
-            FILE  *acessaNumeros;
+            FILE  *acessa_numeros;
 
-            acessaNumeros = fopen("numeros.txt","r+");
+            acessa_numeros = fopen("numeros.txt","r+");
 
             for(int posicao=0; posicao<=qtd_linhas; posicao++){
-                fscanf(acessaNumeros, "%i", &auxNumero);
-                vetorNumeros[posicao] = auxNumero;
+                fscanf(acessa_numeros, "%i", &aux_numero);
+                vetor_numeros[posicao] = aux_numero;
             }
             
-            acessaNumeros = fopen("numeros.txt", "w+");
-            bubble_sort(vetorNumeros, qtd_linhas);
+            acessa_numeros = fopen("numeros.txt", "w+");
+            bubble_sort(vetor_numeros, qtd_linhas);
             
             for(int posicao=0; posicao<qtd_linhas; posicao++){
-                fprintf(acessaNumeros,"%d\n",vetorNumeros[posicao]);
+                fprintf(acessa_numeros,"%d\n",vetor_numeros[posicao]);
             }
 
-            fclose(acessaNumeros);
+            fclose(acessa_numeros);
 
             //Preenchendo vetor de letras
-            FILE *acessaLetras = fopen("letras.txt","r");
+            FILE *acessa_letras = fopen("letras.txt","r");
             
-            int vetorLetras[qtd_linhas];
+            int vetor_letras[qtd_linhas];
             
             for(int posicao=0; posicao<qtd_linhas; posicao++){
-                fgets(caractere,qtd_linhas,acessaLetras);                
-                vetorLetras[posicao]= (int)caractere[0];
+                fgets(caractere,qtd_linhas,acessa_letras);                
+                vetor_letras[posicao]= (int)caractere[0];
             }
 
-            acessaLetras = fopen("letras.txt", "w+");
+            acessa_letras = fopen("letras.txt", "w+");
             
-            bubble_sort(vetorLetras, qtd_linhas);
+            bubble_sort(vetor_letras, qtd_linhas);
 
             for(int posicao=0; posicao<qtd_linhas; posicao++){
-                fprintf(acessaLetras,"%c\n",vetorLetras[posicao]);
+                fprintf(acessa_letras,"%c\n",vetor_letras[posicao]);
             }
             printf("\nArquivos ordenados com sucesso!\n\n");
-            fclose(acessaLetras);
+            fclose(acessa_letras);
         }
     }
-    
     return 0;
 }
